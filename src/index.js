9ebadeb28ad6539b004dc363cf221a5b3a6d2474
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2021-11-15 15:16:00 +0000

    Replace walk-folder-tree with klaw, address some audit security issues

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 const util = require("util");
-const walk = require("walk-folder-tree");
+const walk = require("klaw");
 const shasum = require("shasum");
 
 const readFile = util.promisify(fs.readFile);
 const access = util.promisify(fs.access);
+const through2 = require("through2");
 
 /**
  * Assets hash map library
  *
  * Exports `getHashesMap`, which is used to recursively walk through a
- * directory and generate an Object mapping all matching filepaths to Strings
+ * directory and generate an Object mapping all matching inputPaths to Strings
  * representing the sha-sum of each found file
  */
 
@@ -26,58 +27,76 @@ const access = util.promisify(fs.access);
  * - `basePath` : {String}   Used to output relative paths (default: `null`)
  * - `all`      : {Boolean}  Include hidden files          (default: `false`)
  *
- * @param  {String} filepath
+ * @param  {String} inputPath
  * @param  {Object} options
  * @return {Promise<Object>}
  */
-exports.getHashesMap = function (filepath, options = {}) {
+exports.getHashesMap = function (inputPath, options = {}) {
   const { exclude, extensions } = options;
   const map = {};
   const opts = {};
+  const absoluteInputPath = path.resolve(inputPath);
+  const items = [];
 
   if (options.all) {
     opts.filterFiles = /.*/;
   }
 
-  return access(filepath, fs.constants.F_OK)
+  const processItems = through2.obj(function (item, enc, next) {
+    const ext = item.path.substr(1).split(".").pop();
+    const filename = path.basename(item.path);
+
+    if (
+      (!options.all && filename.startsWith(".")) ||
+      item.stats.isDirectory() ||
+      (exclude && options.exclude.indexOf(ext) !== -1) ||
+      (extensions && extensions.indexOf(ext) === -1)
+    ) {
+      return next();
+    }
+
+    const key = options.absolute
+      ? item.path
+      : path.relative(options.basePath || absoluteInputPath, item.path);
+
+    this.push({ key, path: item.path });
+
+    next();
+  });
+
+  return access(inputPath, fs.constants.F_OK)
     .catch(() => {
-      throw new Error(`Path '${filepath}' not found`);
+      throw new Error(`Path '${inputPath}' not found`);
     })
-    .then(() =>
-      walk(filepath, opts, (file, callback) => {
-        const ext = file.name.substr(1).split(".").pop();
-
-        if (
-          file.directory ||
-          (exclude && options.exclude.indexOf(ext) !== -1) ||
-          (extensions && extensions.indexOf(ext) === -1)
-        ) {
-          return callback();
-        }
-
-        let filepath = options.absolute ? file.fullPath : file.path;
-
-        if (options.basePath) {
-          filepath = path.relative(options.basePath, file.fullPath);
-        }
-
-        return hashFile(file.fullPath)
-          .then((hash) => {
-            map[filepath] = hash;
-            callback();
-          })
-          .catch(callback);
-      }).then(() => map)
-    );
+    .then(
+      () =>
+        new Promise((resolve, reject) => {
+          walk(inputPath, { depthLimit: -1 })
+            .pipe(processItems)
+            .on("data", (item) => items.push(item))
+            .on("end", resolve)
+            .on("error", reject);
+        })
+    )
+    .then(() => {
+      return Promise.all(
+        items.map((item) => {
+          return hashFile(item.path).then((hash) => {
+            map[item.key] = hash;
+          });
+        })
+      );
+    })
+    .then(() => map);
 };
 
 /**
  * Get file hash
  *
  *
- * @param  {String} filepath
+ * @param  {String} inputPath
  * @return {void}
  */
-function hashFile(filepath) {
-  return readFile(filepath, "utf8").then((content) => shasum(content));
+function hashFile(inputPath) {
+  return readFile(inputPath, "utf8").then((content) => shasum(content));
 }
